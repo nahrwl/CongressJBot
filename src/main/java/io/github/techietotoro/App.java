b9a53088e485067eb3bab3dee2aebc1d9b5fa43a
TechieTotoro <techietotoro@gmail.com>
2013-05-13 16:48:27 -1000

    Init

---
+++ src/main/java/io/github/techietotoro/App.java
@@ -0,0 +1,13 @@
+package io.github.techietotoro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
