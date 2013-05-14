c7cb3dea05acc1a16edc89145ec99f9346c141b7
TechieTotoro <techietotoro@gmail.com>
2013-05-13 21:53:30 -1000

    Created basic test plugin.
    It appears that the API is not functioning properly.

--- src/main/java/io/github/techietotoro/App.java
+++ src/main/java/io/github/techietotoro/App.java
@@ -1,13 +1,64 @@
+/* Based off of
+ * the DropboxMirror project
+ * by yetanotherx
+ */
+
 package io.github.techietotoro;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.beust.jcommander.JCommander;
+
+import com.yetanotherx.reddit.RedditPlugin;
+import com.yetanotherx.reddit.api.modules.RedditCore;
+import com.yetanotherx.reddit.api.modules.RedditLink;
+import com.yetanotherx.reddit.util.LinkType;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends RedditPlugin
 {
-    public static void main( String[] args )
+	private Parameters params;
+
+    public App(Parameters params) 
     {
-        System.out.println( "Hello World!" );
+        this.params = params;
+    }
+
+    public void run() throws InterruptedException
+    {
+		RedditCore.newFromUserAndPass(this, params.username, params.password).doLogin();
+        params = null;
+        
+        RedditLink.doSubmit(this, "First post from bot!", "I'm trying to make a nice bot for some guys at futuristparty. Except I don't know python so I can't copy congressbot.", "techietotoro", LinkType.SELF);
+        
+    }
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "JavaCongressWatchBot";
+	}
+
+	@Override
+	public String getVersion() {
+		// TODO Auto-generated method stub
+		return "1.0";
+	}
+	
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+        Parameters params = new Parameters();
+        JCommander jc = new JCommander(params, args);
+        try {
+            new App(params).run();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 }
+
---
+++ src/main/java/io/github/techietotoro/Parameters.java
@@ -0,0 +1,13 @@
+package io.github.techietotoro;
+
+import com.beust.jcommander.Parameter;
+
+public class Parameters {
+    
+    @Parameter(names = { "-user", "-username" }, description = "Username")
+    public String username;
+    
+    @Parameter(names = { "-pass", "-password" }, description = "Password")
+    public String password;
+    
+}
