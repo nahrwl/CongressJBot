12ebd02e83e7cb6cb2159bf45a840906b17c33fb
Nathan Wallace <techietotoro@gmail.com>
2013-08-08 14:01:05 -1000

    Added new bill item class

---
+++ src/main/java/io/github/techietotoro/Item.java
@@ -0,0 +1,53 @@
+package io.github.techietotoro;
+
+public class Item {
+	private String title;
+	private String type;
+	private String number;
+	private String date;
+	private String id;
+	private String link;
+	private String currentStatus;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+	public String getCurrentStatus() {
+		return currentStatus;
+	}
+	public void setCurrentStatus(String currentStatus) {
+		this.currentStatus = currentStatus;
+	}
+}
